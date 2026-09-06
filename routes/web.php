9912750b0e2c98cfd1398951706e81e23b846f7e
Brem0317 <patrickbarcelo17@gmail.com>
2026-09-06 21:35:06 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use App\Http\Controllers\SubmitLeadController;
 use Illuminate\Support\Facades\Route;
 
+// Normal homepage → welcome.tsx
+Route::inertia('/nullypto', 'welcome')->name('home');
+
+// Random redirect
 Route::get('/', function () {
     return rand(0, 1)
         ? redirect('/articles')
         : redirect('/prime-zone');
-})->name('home');
+})->name('landing');
 
 Route::post('/submit-lead', [SubmitLeadController::class, 'store'])
     ->middleware('throttle:10,1')
